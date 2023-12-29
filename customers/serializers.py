3b286882a92650e44ee3f40c3f783479c2c0273f
AneeshNi47 <me@aneeshbharath.com>
2023-12-29 11:51:25 +0400

    added validation optimisation for prescriptions

--- customers/serializers.py
+++ customers/serializers.py
@@ -49,3 +49,46 @@ class PrescriptionSerializer(serializers.ModelSerializer):
         model= Prescription
         exclude = ('organization',)
         read_only_fields = ('organization',)
+
+    class PrescriptionSerializer(serializers.ModelSerializer):
+        customer = serializers.PrimaryKeyRelatedField(queryset=Customer.objects.all(), required=False)
+
+    class Meta:
+        model = Prescription
+        exclude = ('organization',)
+        read_only_fields = ('organization',)
+
+    def to_internal_value(self, data):
+        # Convert integer-like inputs to floats for specific fields
+        float_fields = ['left_sphere', 'right_sphere', 'left_cylinder', 'right_cylinder',
+                        'left_prism', 'right_prism', 'left_add', 'right_add',
+                        'left_ipd', 'right_ipd']
+        for field in float_fields:
+            if field in data and isinstance(data[field], int):
+                data[field] = float(f"{data[field]:.2f}")
+        return super().to_internal_value(data)
+
+    def validate(self, data):
+        # Centralized validation for choice fields
+        choice_fields = {
+            'left_sphere': Prescription.SPHERE_CHOICES,
+            'right_sphere': Prescription.SPHERE_CHOICES,
+            'left_cylinder': Prescription.CYLINDER_CHOICES,
+            'right_cylinder': Prescription.CYLINDER_CHOICES,
+            'left_axis': Prescription.AXIS_CHOICES,
+            'right_axis': Prescription.AXIS_CHOICES,
+            'left_prism': Prescription.PRISM_CHOICES,
+            'right_prism': Prescription.PRISM_CHOICES,
+            'left_add': Prescription.ADD_CHOICES,
+            'right_add': Prescription.ADD_CHOICES
+        }
+        for field, choices in choice_fields.items():
+            if field in data:
+                self._validate_choice_field(data[field], choices)
+        return data
+
+    def _validate_choice_field(self, value, choices):
+        if value is None:
+            return
+        if value not in dict(choices):
+            raise serializers.ValidationError(f"Invalid choice for {value}.")
